55d5f9d9e6fea84cff26692d4123d01de4948292
veda-vi <75447508+veda-vi@users.noreply.github.com>
2021-06-08 21:43:49 -0500

    Update fortify.php

--- config/fortify.php
+++ config/fortify.php
@@ -132,7 +132,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        /*Features::registration(),*/
         Features::resetPasswords(),
         env('MAIL_VERIFY', true) ? Features::emailVerification() : null,
         Features::updateProfileInformation(),
